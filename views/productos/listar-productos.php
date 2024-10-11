56e2ca143a17724d460c0adc7654ca090e19c74e
BrRyMt <Brayan.ms.tyco@gmail.com>
2024-10-11 11:33:48 -0500

    Agredo las vistas de cada modulo

--- views/productos/listar-productos.php
+++ views/productos/listar-productos.php
@@ -1,3 +1,30 @@
-<div class="container-fluid px-4">
-    <h1 class="mt-4">Registrar productos</h1>
+<?php
+//Parámetros de la aplicacion
+require_once "../../app/config/App.php";
+?>
+
+<?php require_once "../includes/header.php" ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <?php echo renderContentHeader("Productos", "Inicio") ?>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- Contenido Personalizado -->
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
 </div>
+<!-- /.content-wrapper -->
+
+<?php require_once "../includes/footer.php" ?>
+</body>
+
+</html>
